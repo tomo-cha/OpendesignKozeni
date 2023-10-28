9647e3aae03490472be1c2130b99675025f8e33c
amanya <corotanman@gmail.com>
2023-10-28 14:08:14 +0900

    APIの情報修正

--- docs/calenderApi.js
+++ docs/calenderApi.js
@@ -1,12 +1,13 @@
 
+
+
 function init() {
     gapi.client.init({
         'apiKey': 'AIzaSyBulbUZ6M_i2u2R_vuCcDGEDb5Pf48SsFQ',
-        'clientId': '593980784395-mcopk6oi9gc419ognfb8nulkm58j8838.apps.googleusercontent.com',
-        'scope': 'https://www.googleapis.com/auth/calendar',
-        'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest']
+        'clientId': '593980784395-mcopk6oi9gc419ognfb8nulkm58j8838.apps.googleusercontent.com'
+        // 'scope': 'https://www.googleapis.com/auth/calendar',
+        // 'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest']
     }).then(function () {
-        // APIの初期化が完了した後の処理をここに書きます。
     });
 }
 
