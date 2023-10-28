ee3dad5e253615c1751dcda59f1237572b0a8937
amanya <corotanman@gmail.com>
2023-10-28 14:16:34 +0900

    コーディングをgooglecalenderのリファレンスに依拠

--- docs/calenderApi.js
+++ docs/calenderApi.js
@@ -1,32 +1,129 @@
+const CLIENT_ID = '593980784395-mcopk6oi9gc419ognfb8nulkm58j8838.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyBulbUZ6M_i2u2R_vuCcDGEDb5Pf48SsFQ';
 
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
 
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
 
-function init() {
-    gapi.client.init({
-        'apiKey': 'AIzaSyBulbUZ6M_i2u2R_vuCcDGEDb5Pf48SsFQ',
-        'clientId': '593980784395-mcopk6oi9gc419ognfb8nulkm58j8838.apps.googleusercontent.com'
-        // 'scope': 'https://www.googleapis.com/auth/calendar',
-        // 'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest']
-    }).then(function () {
-    });
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
+
+document.getElementById('authorize_button').style.visibility = 'hidden';
+document.getElementById('signout_button').style.visibility = 'hidden';
+
+/**
+ * Callback after api.js is loaded.
+ */
+function gapiLoaded() {
+gapi.load('client', initializeGapiClient);
 }
 
-// APIクライアントとOAuth2ライブラリのロード
-function loadClient() {
-    gapi.client.setApiKey("YOUR_API_KEY");
-    return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); });
+/**
+ * Callback after the API client is loaded. Loads the
+ * discovery doc to initialize the API.
+ */
+async function initializeGapiClient() {
+await gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: [DISCOVERY_DOC],
+});
+gapiInited = true;
+maybeEnableButtons();
 }
 
+/**
+ * Callback after Google Identity Services are loaded.
+ */
+function gisLoaded() {
+tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    callback: '', // defined later
+});
+gisInited = true;
+maybeEnableButtons();
+}
+
+/**
+ * Enables user interaction after all libraries are loaded.
+ */
+function maybeEnableButtons() {
+if (gapiInited && gisInited) {
+    document.getElementById('authorize_button').style.visibility = 'visible';
+}
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
 function handleAuthClick() {
-    gapi.auth2.getAuthInstance().signIn().then(function() {
-        console.log("User signed in.");
-    });
+tokenClient.callback = async (resp) => {
+    if (resp.error !== undefined) {
+    throw (resp);
+    }
+    document.getElementById('signout_button').style.visibility = 'visible';
+    document.getElementById('authorize_button').innerText = 'Refresh';
+    await listUpcomingEvents();
+};
+
+if (gapi.client.getToken() === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
+    tokenClient.requestAccessToken({prompt: 'consent'});
+} else {
+    // Skip display of account chooser and consent dialog for an existing session.
+    tokenClient.requestAccessToken({prompt: ''});
+}
 }
 
+/**
+ *  Sign out the user upon button click.
+ */
 function handleSignoutClick() {
-    gapi.auth2.getAuthInstance().signOut().then(function() {
-        console.log("User signed out.");
-    });
+const token = gapi.client.getToken();
+if (token !== null) {
+    google.accounts.oauth2.revoke(token.access_token);
+    gapi.client.setToken('');
+    document.getElementById('content').innerText = '';
+    document.getElementById('authorize_button').innerText = 'Authorize';
+    document.getElementById('signout_button').style.visibility = 'hidden';
+}
+}
+
+/**
+ * Print the summary and start datetime/date of the next ten events in
+ * the authorized user's calendar. If no events are found an
+ * appropriate message is printed.
+ */
+async function listUpcomingEvents() {
+let response;
+try {
+    const request = {
+    'calendarId': 'primary',
+    'timeMin': (new Date()).toISOString(),
+    'showDeleted': false,
+    'singleEvents': true,
+    'maxResults': 10,
+    'orderBy': 'startTime',
+    };
+    response = await gapi.client.calendar.events.list(request);
+} catch (err) {
+    document.getElementById('content').innerText = err.message;
+    return;
+}
+
+const events = response.result.items;
+if (!events || events.length == 0) {
+    document.getElementById('content').innerText = 'No events found.';
+    return;
+}
+// Flatten to string to display
+const output = events.reduce(
+    (str, event) => `${str}${event.summary} (${event.start.dateTime || event.start.date})\n`,
+    'Events:\n');
+document.getElementById('content').innerText = output;
 }
