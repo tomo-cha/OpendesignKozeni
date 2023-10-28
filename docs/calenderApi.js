a02af1c8388256e92ac2d08177fdd69340cbdc6e
amanya <corotanman@gmail.com>
2023-10-28 18:17:21 +0900

    apiを使用して予定を取れるスクリプトcalenderApi.jsを記述

--- docs/calenderApi.js
+++ docs/calenderApi.js
@@ -1,11 +1,5 @@
-// document.addEventListener('DOMContentLoaded', (event) => {
-//     document.getElementById('authorize_button').style.visibility = 'hidden';
-//     document.getElementById('signout_button').style.visibility = 'hidden';
-// });
-
 const CLIENT_ID = '593980784395-mcopk6oi9gc419ognfb8nulkm58j8838.apps.googleusercontent.com';
 const API_KEY = 'AIzaSyBulbUZ6M_i2u2R_vuCcDGEDb5Pf48SsFQ';
-
 // Discovery doc URL for APIs used by the quickstart
 const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
 
@@ -66,27 +60,23 @@ if (gapiInited && gisInited) {
  *  Sign in the user upon button click.
  */
 function handleAuthClick() {
-    if (typeof gapi === 'undefined' || !gapi.client) {
-        alert('Google APIがまだロードされていません。しばらく待ってから再試行してください。');
-        return;
+tokenClient.callback = async (resp) => {
+    if (resp.error !== undefined) {
+    throw (resp);
     }
+    document.getElementById('signout_button').style.visibility = 'visible';
+    document.getElementById('authorize_button').innerText = 'Refresh';
+    await listUpcomingEvents();
+};
 
-    tokenClient.callback = async (resp) => {
-        if (resp.error !== undefined) {
-            throw (resp);
-        }
-        document.getElementById('signout_button').style.visibility = 'visible';
-        document.getElementById('authorize_button').innerText = 'Refresh';
-        await listUpcomingEvents();
-    };
-    if (gapi.client.getToken() === null) {
-        // Prompt the user to select a Google Account and ask for consent to share their data
-        // when establishing a new session.
-        tokenClient.requestAccessToken({prompt: 'consent'});
-    } else {
-        // Skip display of account chooser and consent dialog for an existing session.
-        tokenClient.requestAccessToken({prompt: ''});
-    }
+if (gapi.client.getToken() === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
+    tokenClient.requestAccessToken({prompt: 'consent'});
+} else {
+    // Skip display of account chooser and consent dialog for an existing session.
+    tokenClient.requestAccessToken({prompt: ''});
+}
 }
 
 /**
