f113c19934d86c3e4ff8711354486b35fe577178
amanya <corotanman@gmail.com>
2023-10-28 18:53:32 +0900

    ワードが一致しているかを検索する機能を搭載

--- docs/calenderApi.js
+++ docs/calenderApi.js
@@ -3,8 +3,6 @@ const API_KEY = 'AIzaSyBulbUZ6M_i2u2R_vuCcDGEDb5Pf48SsFQ';
 // Discovery doc URL for APIs used by the quickstart
 const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
 
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
 const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
 
 let tokenClient;
@@ -14,11 +12,8 @@ let gisInited = false;
 document.getElementById('authorize_button').style.visibility = 'hidden';
 document.getElementById('signout_button').style.visibility = 'hidden';
 
-/**
- * Callback after api.js is loaded.
- */
 function gapiLoaded() {
-gapi.load('client', initializeGapiClient);
+    gapi.load('client', initializeGapiClient);
 }
 
 /**
@@ -26,71 +21,121 @@ gapi.load('client', initializeGapiClient);
  * discovery doc to initialize the API.
  */
 async function initializeGapiClient() {
-await gapi.client.init({
-    apiKey: API_KEY,
-    discoveryDocs: [DISCOVERY_DOC],
-});
-gapiInited = true;
-maybeEnableButtons();
+    await gapi.client.init({
+        apiKey: API_KEY,
+        discoveryDocs: [DISCOVERY_DOC],
+    });
+    gapiInited = true;
+    maybeEnableButtons();
 }
 
 /**
  * Callback after Google Identity Services are loaded.
  */
 function gisLoaded() {
-tokenClient = google.accounts.oauth2.initTokenClient({
-    client_id: CLIENT_ID,
-    scope: SCOPES,
-    callback: '', // defined later
-});
-gisInited = true;
-maybeEnableButtons();
+    tokenClient = google.accounts.oauth2.initTokenClient({
+        client_id: CLIENT_ID,
+        scope: SCOPES,
+        callback: '', // defined later
+    });
+    gisInited = true;
+    maybeEnableButtons();
 }
 
 /**
  * Enables user interaction after all libraries are loaded.
  */
 function maybeEnableButtons() {
-if (gapiInited && gisInited) {
-    document.getElementById('authorize_button').style.visibility = 'visible';
-}
+    if (gapiInited && gisInited) {
+        document.getElementById('authorize_button').style.visibility = 'visible';
+    }
 }
 
 /**
  *  Sign in the user upon button click.
  */
 function handleAuthClick() {
-tokenClient.callback = async (resp) => {
-    if (resp.error !== undefined) {
-    throw (resp);
-    }
-    document.getElementById('signout_button').style.visibility = 'visible';
-    document.getElementById('authorize_button').innerText = 'Refresh';
-    await listUpcomingEvents();
-};
+    tokenClient.callback = async (resp) => {
+        if (resp.error !== undefined) {
+            throw (resp);
+        }
+        document.getElementById('signout_button').style.visibility = 'visible';
+        document.getElementById('authorize_button').innerText = 'Refresh';
+        await listUpcomingEvents();
+    };
 
-if (gapi.client.getToken() === null) {
-    // Prompt the user to select a Google Account and ask for consent to share their data
-    // when establishing a new session.
-    tokenClient.requestAccessToken({prompt: 'consent'});
-} else {
-    // Skip display of account chooser and consent dialog for an existing session.
-    tokenClient.requestAccessToken({prompt: ''});
-}
+    if (gapi.client.getToken() === null) {
+        // Prompt the user to select a Google Account and ask for consent to share their data
+        // when establishing a new session.
+        tokenClient.requestAccessToken({ prompt: 'consent' });
+    } else {
+        // Skip display of account chooser and consent dialog for an existing session.
+        tokenClient.requestAccessToken({ prompt: '' });
+    }
 }
 
 /**
  *  Sign out the user upon button click.
  */
 function handleSignoutClick() {
-const token = gapi.client.getToken();
-if (token !== null) {
-    google.accounts.oauth2.revoke(token.access_token);
-    gapi.client.setToken('');
-    document.getElementById('content').innerText = '';
-    document.getElementById('authorize_button').innerText = 'Authorize';
-    document.getElementById('signout_button').style.visibility = 'hidden';
+    const token = gapi.client.getToken();
+    if (token !== null) {
+        google.accounts.oauth2.revoke(token.access_token);
+        gapi.client.setToken('');
+        document.getElementById('content').innerText = '';
+        document.getElementById('authorize_button').innerText = 'Authorize';
+        document.getElementById('signout_button').style.visibility = 'hidden';
+    }
 }
+
+/**
+ * Print the summary and start datetime/date of the next ten events in
+ * the authorized user's calendar. If no events are found an
+ * appropriate message is printed.
+ */
+
+//ユーザのイベントをlistにする
+async function listUpcomingEvents() {
+    let response;
+    try {
+        const request = {
+            'calendarId': 'primary',
+            'timeMin': (new Date()).toISOString(),
+            'showDeleted': false,
+            'singleEvents': true,
+            'maxResults': 10,
+            'orderBy': 'startTime',
+        };
+        response = await gapi.client.calendar.events.list(request);
+    } catch (err) {
+        document.getElementById('content').innerText = err.message;
+        return;
+    }
+
+    const events = response.result.items;
+    if (!events || events.length == 0) {
+        document.getElementById('content').innerText = 'No events found.';
+        return;
+    }
+    // Flatten to string to display
+    const output = events.reduce(
+        (str, event) => `${str}${event.summary} (${event.start.dateTime || event.start.date})\n`,
+        'Events:\n');
+    document.getElementById('content').innerText = output;
+}
+
+//予定名に特定の単語が含まれているかを検索するための関数
+function containsKeyword(summary, keyword) {
+    return summary.toLowerCase().includes(keyword.toLowerCase());
+}
+
+// ... [省略されたコード]
+
+/**
+ * Check if an event summary contains the keyword.
+ */
+function containsKeyword(summary, keyword) {
+    return summary.toLowerCase().includes(keyword.toLowerCase());
 }
 
 /**
@@ -99,30 +144,38 @@ if (token !== null) {
  * appropriate message is printed.
  */
 async function listUpcomingEvents() {
-let response;
-try {
-    const request = {
-    'calendarId': 'primary',
-    'timeMin': (new Date()).toISOString(),
-    'showDeleted': false,
-    'singleEvents': true,
-    'maxResults': 10,
-    'orderBy': 'startTime',
-    };
-    response = await gapi.client.calendar.events.list(request);
-} catch (err) {
-    document.getElementById('content').innerText = err.message;
-    return;
-}
+    let response;
+    try {
+        const request = {
+            'calendarId': 'primary',
+            'timeMin': (new Date()).toISOString(),
+            'showDeleted': false,
+            'singleEvents': true,
+            'maxResults': 10,
+            'orderBy': 'startTime',
+        };
+        response = await gapi.client.calendar.events.list(request);
+    } catch (err) {
+        document.getElementById('content').innerText = err.message;
+        return;
+    }
 
-const events = response.result.items;
-if (!events || events.length == 0) {
-    document.getElementById('content').innerText = 'No events found.';
-    return;
-}
-// Flatten to string to display
-const output = events.reduce(
-    (str, event) => `${str}${event.summary} (${event.start.dateTime || event.start.date})\n`,
-    'Events:\n');
-document.getElementById('content').innerText = output;
+    const events = response.result.items;
+    if (!events || events.length == 0) {
+        document.getElementById('content').innerText = 'No events found.';
+        return;
+    }
+
+    const output = events.reduce(
+        (str, event) => `${str}${event.summary} (${event.start.dateTime || event.start.date})\n`,
+        'Events:\n');
+
+    const meetingsOutput = events
+        .filter(event => containsKeyword(event.summary, "meeting"))
+        .reduce(
+            (str, event) => `${str}${event.start.dateTime || event.start.date}\n`,
+            'Meeting Times:\n');
+
+    document.getElementById('content').innerText = output;
+    document.getElementById('meetingTimes').innerText = meetingsOutput; // こちらの要素をHTMLに追加する必要があります
 }
