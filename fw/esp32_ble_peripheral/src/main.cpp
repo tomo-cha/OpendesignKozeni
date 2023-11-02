489be86517ae397ae5fa76a11a15d4a7e2a50495
amanya <corotanman@gmail.com>
2023-11-02 11:55:28 +0900

    add ウェブからrealtimeの時刻取得できる機能

--- fw/esp32_ble_peripheral/src/main.cpp
+++ fw/esp32_ble_peripheral/src/main.cpp
@@ -21,7 +21,7 @@ int servoPin = 32;
 const int _LED_PIN = 2;
 const String BLEName = "XIAO_ESP32BLE";
 
-Servo myservo;   
+Servo myservo;
 
 class MyServerCallbacks : public BLEServerCallbacks
 {
@@ -42,31 +42,84 @@ class MyServerCallbacks : public BLEServerCallbacks
   }
 };
 
-//右にスイープして戻す
+// 現在時刻を保存するためのグローバル変数
+int receivedHour = 0, receivedMinute = 0, receivedSecond = 0;
+bool updateReceivedTime = false;
+
+// 現在時刻を受け付けるためのコールバック
+class MyCharacteristicCallbacks : public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *pCharacteristic)
+  {
+    std::string value = pCharacteristic->getValue();
+
+    if (value.length() > 0)
+    {
+      sscanf(value.c_str(), "%02d:%02d:%02d", &receivedHour, &receivedMinute, &receivedSecond);
+
+      // 時刻更新のフラグをセット
+      updateReceivedTime = true;
+
+      Serial.println("Received Time: " + String(receivedHour) + ":" + String(receivedMinute) + ":" + String(receivedSecond));
+    }
+  }
+};
+
+// 　取得した時間をプリントするような処理
+void timePrint()
+{
+  if (updateReceivedTime)
+  {
+    delay(1000); // 1秒待機
+
+    // 秒を更新
+    receivedSecond++;
+    if (receivedSecond >= 60)
+    {
+      receivedSecond = 0;
+      receivedMinute++;
+      if (receivedMinute >= 60)
+      {
+        receivedMinute = 0;
+        receivedHour++;
+        if (receivedHour >= 24)
+        {
+          receivedHour = 0;
+        }
+      }
+    }
+
+    // 更新された時刻をシリアルに出力
+    Serial.printf("%02d:%02d:%02d\n", receivedHour, receivedMinute, receivedSecond);
+  }
+}
+
+// 右にスイープして戻す
 void moveServoRight()
 {
-  for (int i = 40; i < 120; i+=2)
+  for (int i = 40; i < 120; i += 2)
   {
-    myservo.write(i);  // サーボを0度の位置に動かす
+    myservo.write(i); // サーボを0度の位置に動かす
     delay(30);        // 0.5秒待つ
   }
 
-  for (int i = 120; i > 40; i-=2)
+  for (int i = 120; i > 40; i -= 2)
   {
-    myservo.write(i);  // サーボを0度の位置に動かす
+    myservo.write(i); // サーボを0度の位置に動かす
     delay(50);        // 0.5秒待つ
   }
 }
 
 void setup()
 {
-  Serial.begin(9600);     
+  Serial.begin(9600);
 
   // Create the BLE Device
   BLEDevice::init(BLEName.c_str());
 
   // Create the BLE Server
   BLEServer *pServer = BLEDevice::createServer();
+
   pServer->setCallbacks(new MyServerCallbacks());
 
   // Create the BLE Service
@@ -79,6 +132,9 @@ void setup()
           BLECharacteristic::PROPERTY_WRITE |
           BLECharacteristic::PROPERTY_NOTIFY |
           BLECharacteristic::PROPERTY_INDICATE);
+
+  // ここでコールバックをセットします
+  pCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
 
   // Create a BLE Descriptor
   pCharacteristic->addDescriptor(new BLE2902());
@@ -93,32 +149,37 @@ void setup()
   pinMode(_LED_PIN, OUTPUT);
 
   myservo.attach(servoPin);
-  myservo.write(40); 
-
+  myservo.write(40);
 }
 
 void loop()
 {
   if (deviceConnected)
   {
-    Serial.print(".");
+    // Serial.print(".");
     uint8_t *pData = pCharacteristic->getData();
     if (pData[0] == 'A')
     {
       digitalWrite(_LED_PIN, HIGH);
+      Serial.print("recieve this : ");
+      Serial.println(pData[0]);
     }
     else if (pData[0] == 'B')
     {
-      // servoを右に動かす
       moveServoRight();
       globalValue += 1;
+
       digitalWrite(_LED_PIN, LOW);
+      Serial.print("recieve this : ");
+      Serial.println(pData[0]);
     }
     else if (pData[0] == 'C')
     {
-      // servoを左に動かす
       globalValue -= 1;
       digitalWrite(_LED_PIN, LOW);
+
+      Serial.print("recieve this : ");
+      Serial.println(pData[0]);
     }
     else
     {
@@ -127,4 +188,7 @@ void loop()
     pCharacteristic->notify();
   }
   delay(100);
+
+  //BLEと一度接続された後にその時刻を継続的に吐き出す処理
+  timePrint();
 }
